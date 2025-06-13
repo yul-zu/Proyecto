70d1d32a3b6f51b55251335031ec578c9f52e713
yul_zu <yulianakarime.zunigamanzano.gen23@cbtis105.edu.mx>
2025-06-13 08:43:01 -0600

    PARTE 3 TERMINADA

--- ClientApp/Usuarios.tsx
+++ ClientApp/Usuarios.tsx
@@ -1,29 +1,128 @@
+import { useEffect, useState } from "react";
+
+type Usuario = {
+    "Id": string,
+    "Nombre": string,
+    "Password": string,
+    "Correo": string
+}
+
 const Usuarios = () => {
+    const [registros, setRegistros] = useState<Usuario[]>([]);
+    const [texto, setTexto] = useState("");
+
+    const listarRegistros = async () => {
+        let url = "/api/usuarios";
+       
+       if(texto){
+        url += "?texto=" + texto;
+       }
+        const resp = await fetch(url);
+        if (resp.ok) {
+            const datos = await resp.json();
+            setRegistros(datos);
+        }
+    }
+
+    useEffect(()=>{
+       listarRegistros();
+    }, []);
+    const buscar = () => {
+        listarRegistros();
+    }
+
+    const eliminar = async (item:Usuario) => {
+       if(!confirm("Deseas eliminar el usuario" + item.Nombre + "?")){
+        return;
+       }
+
+       const resp  = await fetch ("/api/usuarios/" + item.Id, {
+        method: "DELETE"
+       });
+
+       if(resp.ok){
+        listarRegistros();
+       }
+       else{
+        const error = await resp.text();
+        console.log(error);
+        alert("Ocurrio un error al eliminar el registro")
+       }
+    }
+
     return (
         <>
             <div className="container">
                 <h1>Usuarios</h1>
             </div>
             <div className="container">
-    <div className="card">
-        <div className="card-header">Búsqueda</div>
-        <div className="card-body">
-            <div className="row">
-                <div className="col-12">
-                    <div className="mb-3">
-                        <label>Búsqueda de Usuarios</label>
-                        <input type="text" className="form-control"  placeholder="Introduce el nombre o el correo"/>
+                <div className="card">
+                    <div className="card-header">Busqueda</div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="mb-3">
+                                    <label>Busqueda de Usuarios</label>
+                                    <input type="text" className="form-control" 
+                                    placeholder="Introduce el nombre" 
+                                    onChange={(e) => setTexto(e.target.value)}/>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <button className="btn btn-primary" onClick={buscar}>Buscar</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="col-12">
-                    <button className="btn btn-primary">Buscar</button>
+            </div>
+
+            <div className="container mt-4">
+                <div className="card">
+                    <div className="card-header">Usuarios existentes</div>
+                    <div className="card-body">
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Nombre</th>
+                                    <th>Correo</th>
+                                    <th>Password</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            {
+                                registros.length === 0 &&
+                                <tbody>
+                                    <tr>
+                                        <td colSpan={5}>No hay registros por mostrar</td>
+                                    </tr>
+                                </tbody>
+                            }
+                            {
+                                registros.length > 0 &&
+                                <tbody>
+                                    {
+                                        registros.map((item, index) => (
+                                            <tr key={item.Id}>
+                                                <td>{index + 1}</td>
+                                                <td>{item.Nombre}</td>
+                                                <td>{item.Correo}</td>
+                                                <td>{item.Password}</td>
+                                                <td className="d-flex gap-2">
+                                                    <a className="btn btn-primary" href={"/usuarios/" + item.Id}>Editar</a>
+                                                    <button className="btn btn-danger" onClick={()=> eliminar(item)}>Eliminar</button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            }
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
         </>
-    );
+    )
 }
 
 export default Usuarios;
--- Controllers/Api/ApiUsuariosController.cs
+++ Controllers/Api/ApiUsuariosController.cs
@@ -1,33 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
-using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/usuarios")]
+
 public class ApiUsuariosController : ControllerBase
 {
-    // Métodos para hacer las operaciones CRUD
-    // C = Create
-    // R = Read
-    // U = Update
-    // D = Delete
 
-    private readonly IMongoCollection<Usuario> collection;
 
-public ApiUsuariosController()
-{
+
+
+
+  private readonly IMongoCollection<Usuario> collection;
+  public ApiUsuariosController()
+  {
     var client = new MongoClient(CadenasConexion.MONGO_DB);
     var database = client.GetDatabase("Escuela_Yuliana_Danna");
     this.collection = database.GetCollection<Usuario>("Usuarios");
-}
 
-[HttpGet]
-public IActionResult ListarUsuarios()
-{
+  }
+
+  [HttpGet]
+  public IActionResult ListarUsuarios(string? texto)
+  {
     var filter = FilterDefinition<Usuario>.Empty;
+    if (!string.IsNullOrWhiteSpace(texto))
+    {
+      var filterNombre = Builders<Usuario>.Filter.Regex(u => u.Nombre, new BsonRegularExpression(texto, "i"));
+      var filterCorreo = Builders<Usuario>.Filter.Regex(u => u.Correo, new BsonRegularExpression(texto, "i"));
+      filter = Builders<Usuario>.Filter.Or(filterNombre, filterCorreo);
+    }
     var list = this.collection.Find(filter).ToList();
     return Ok(list);
-}
+  }
 
-    
+[HttpDelete("{id}")]
+  public IActionResult Delete(string id)
+  {
+    var filter = Builders<Usuario>.Filter.Eq(x => x.Id, id);
+    var item = this.collection.Find(filter).FirstOrDefault();
+    if (item != null)
+    {
+      this.collection.DeleteOne(filter);
+    }
+    return NoContent();
+  }
 }
